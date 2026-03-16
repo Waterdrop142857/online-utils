f3dd46e5a0652cc76cc73a88011a4ab7b8c895a6
Waterdrop142857 <waterdrop142857@outlook.com>
2026-03-16 13:04:01 +0800

    重新修正了Github Pages的资源路径

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,13 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import vue from '@vitejs/plugin-vue'
 
 // https://vite.dev/config/
-export default defineConfig({
-  base: process.env.VITE_BASE || '/',
-  plugins: [vue()],
+export default defineConfig(({ mode }) => {
+  // 加载所有环境变量，包括系统环境变量
+  const env = loadEnv(mode, process.cwd(), '')
+  
+  return {
+    base: env.VITE_BASE || '/',
+    plugins: [vue()],
+  }
 })
